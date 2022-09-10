c12c4e4df2c6b53ca08f8975eac4bf64840f9995
ddongseop <dlehdtjq00o@gmail.com>
2022-09-10 21:32:43 +0900

    before use spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +12,20 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig { //배우가 상대 배우를 선택하는게 아니라, 담당자를 따로 두는 것
 
+    //누가 선택되는지 깔끔하게 정리됨 (리팩토링)
+    private static MemberRepository memoryRepository() {
+        return new MemoryMemberRepository();
+    }
+    private static DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository()); //생성자 주입, DI(의존관계 주입)
+        return new MemberServiceImpl(memoryRepository()); //생성자 주입, DI(의존관계 주입)
     }
 
     public OrderService orderService(){ //배우의 역할에 사람이 캐스팅 되는 것과 비슷
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memoryRepository(), discountPolicy());
     }
-
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,7 +11,7 @@ public class OrderServiceImpl implements OrderService{
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //OCP 위반
+//    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //OCP 위반 -> 클라이언트 코드를 안바꿔도 되면 OK
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
